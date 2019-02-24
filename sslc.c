98cf1f76deb82d07c0e2c5034593e2d7dac84ce9
Nicolas Sauzede <nsauzede@laposte.net>
2019-02-25 00:03:52 +0100

    Indent and whitespace police

--- sslc.c
+++ sslc.c
@@ -9,82 +9,72 @@
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 
-int main( int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	int port = 10001;
 	int n;
 	int s;
 
-	if (argc > 1)
-	{
-		sscanf( argv[1], "%d", &port);
+	if (argc > 1) {
+		sscanf(argv[1], "%d", &port);
 	}
 
 	SSL_library_init();
 	SSL_load_error_strings();
 
-	s = socket( PF_INET, SOCK_STREAM, 0);
-	if (s != -1)
-	{
+	s = socket(PF_INET, SOCK_STREAM, 0);
+	if (s != -1) {
 		struct sockaddr_in sa;
 		char *host = "127.0.0.1";
 
-		memset( &sa, 0, sizeof( sa));
+		memset(&sa, 0, sizeof(sa));
 		sa.sin_family = AF_INET;
-		sa.sin_port = htons( port);
-		sa.sin_addr.s_addr = inet_addr( host);
-		printf( "connecting on port %d..\n", port);
-		if (!connect( s, (struct sockaddr *)&sa, sizeof( sa)))
-		{
+		sa.sin_port = htons(port);
+		sa.sin_addr.s_addr = inet_addr(host);
+		printf("connecting on port %d..\n", port);
+		if (!connect(s, (struct sockaddr *)&sa, sizeof(sa))) {
 			SSL_CTX *ssl_ctx = 0;
 			SSL *ssl = 0;
 
-			ssl_ctx = SSL_CTX_new( SSLv23_client_method());
-			if (ssl_ctx)
-			{
-				ssl = SSL_new( ssl_ctx);
-				if (ssl)
-				{
-					if (SSL_set_fd( ssl, s))
-					{
-						printf( "SSL connecting..\n");
-						n = SSL_connect( ssl);
-						if (n == 1)
-						{
+			ssl_ctx = SSL_CTX_new(SSLv23_client_method());
+			if (ssl_ctx) {
+				ssl = SSL_new(ssl_ctx);
+				if (ssl) {
+					if (SSL_set_fd(ssl, s)) {
+						printf("SSL connecting..\n");
+						n = SSL_connect(ssl);
+						if (n == 1) {
 							char buf[1024];
 
-							printf( "ssl initiated\n");
-							n = snprintf( buf, sizeof( buf), "hello ssl\n");
-							n = SSL_write( ssl, buf, n);
-							printf( "SSL_write %d bytes\n", n);
+							printf("ssl initiated\n");
+							n = snprintf(buf, sizeof(buf), "hello ssl\n");
+							n = SSL_write(ssl, buf, n);
+							printf("SSL_write %d bytes\n", n);
 						}
-						else
-						{
+						else {
 #ifdef WIN32
 							unsigned long err = ERR_get_error();
-							printf( "failed to SSL connect (%d, %d) (%ld, %s)\n", n, SSL_get_error( ssl, n), err, ERR_error_string( err, NULL));
+							printf("failed to SSL connect (%d, %d) (%ld, %s)\n", n, SSL_get_error(ssl, n), err, ERR_error_string(err, NULL));
 #else
-							printf( "failed to SSL connect (%d)\n", n);
+							printf("failed to SSL connect (%d)\n", n);
 #endif
 						}
 					}
 					else
-						printf( "failed to SSL set fd\n");
-					SSL_shutdown( ssl);
+						printf("failed to SSL set fd\n");
+					SSL_shutdown(ssl);
 				}
 				else
-					printf( "couldn't create server ssl\n");
-				SSL_CTX_free( ssl_ctx);
+					printf("couldn't create server ssl\n");
+				SSL_CTX_free(ssl_ctx);
 			}
 			else
-				printf( "couldn't create server ctx\n");
+				printf("couldn't create server ctx\n");
 		}
 		else
-			perror( "connect");
+			perror("connect");
 	}
 	else
-		perror( "socket");
+		perror("socket");
 
 	return 0;
 }
-
